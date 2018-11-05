9d5750f1358518c2d4d078a26fb9371f758ebae8
ilves <ilves@ilves-Lenovo-G50-70>
2018-11-05 17:10:23 +0200

    Added function that displays useful column name, data type and NA info.

--- funcs.R
+++ funcs.R
@@ -11,3 +11,21 @@ f <- function(x) {
   path <- '/home/ilves/Documents/R_Rstudio/R/data_files/'
   paste(path, x, sep='')
 }
+
+
+# Function to display DF of dataframe column names and their types.
+# In addition to print the unique data type values and number of NA-s.
+
+info <- function(df) {
+  # Parameters:
+  # df : data.frame
+  # Returns : df of column names and their types + info about
+  #           unique data types and number of NA values
+  
+  name_type <- data.frame(names(df), as.vector(sapply(df, typeof)))
+  colnames(name_type) <- c("column name", "data type")
+  nas <- sum(is.na(df))
+  unique_dtypes <- as.vector(unique(name_type$`data type`))
+  cat("Data types:", unique_dtypes, fill = TRUE)
+  cat("Total number of NA-s:", nas, fill = TRUE)
+}
